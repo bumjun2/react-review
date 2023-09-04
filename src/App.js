1fda7d2730ad454fb4a0064ffa1e5efe42fa64c8
bumjun2 <jbj3713@naver.com>
2023-09-04 22:20:44 +0900

    jsx - props기초

--- src/App.js
+++ src/App.js
@@ -3,9 +3,40 @@ import React from 'react';
 import ExpensItem from './Components/ExpensItem';
 
 const App = () => {
+  const expenses = [
+    {
+      title: '바나나',
+      price: 2000,
+      date: new Date(2023, 5, 3),
+    },
+    {
+      title: '샤인머스캣',
+      price: 5000,
+      date: new Date(2023, 12, 4),
+    },
+    {
+      title: '멜론',
+      price: 5000,
+      date: new Date(2023, 1, 23),
+    },
+  ];
   return (
     <>
-      <ExpensItem title="샤인머스캣" />
+      <ExpensItem
+        title={expenses[0].title}
+        price={expenses[0].price}
+        date={expenses[0].date}
+      />
+      <ExpensItem
+        title={expenses[1].title}
+        price={expenses[1].price}
+        date={expenses[1].date}
+      />
+      <ExpensItem
+        title={expenses[2].title}
+        price={expenses[2].price}
+        date={expenses[2].date}
+      />
     </>
   );
 };
--- src/Components/ExpensItem.js
+++ src/Components/ExpensItem.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './ExpensItem.css';
 
-const ExpensItem = () => {
-  const expenseDate = new Date(2023, 8, 4);
-  const expenseTitle = '냠냠치킨';
-  const expensePrice = 3000;
+const ExpensItem = ({ title, date, price }) => {
+  // const expenseDate = date;
+  // const expenseTitle = title;
+  // const expensePrice = price;
 
   //1자리 숫자를 2자리 숫자로 만드는 변수
   const make2digit = (text) => {
@@ -13,19 +13,19 @@ const ExpensItem = () => {
 
   //날짜 날짜 변환 함수 정의
   const Dates = () => {
-    const year = expenseDate.getFullYear();
-    const month = expenseDate.getMonth();
-    const date = expenseDate.getDate();
+    const year = date.getFullYear();
+    const month = date.getMonth();
+    const day = date.getDate();
 
-    return `${year}년 ${make2digit(month)}월 ${make2digit(date)}일`;
+    return `${year}년 ${make2digit(month)}월 ${make2digit(day)}일`;
   };
 
   return (
     <div className="expense-item">
       <div>{Dates()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">{expensePrice}원</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">{price}원</div>
       </div>
     </div>
   );
