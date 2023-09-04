3978838b84d14c38366e31cf7b0991edf749c937
bumjun2 <jbj3713@naver.com>
2023-09-04 21:38:26 +0900

    jsx - 컴퍼넌트 기초

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import './App.css';
 import React from 'react';
+import AppItem from './AppItem';
+import NoName from './NoName';
+import Hello from './Hello';
 
-function App() {
-  // const $h2 = React.createElement('h2', null, '방가방가');
-  const $h2 = <h2>반가워요 !</h2>;
-
+const App = () => {
   return (
     <>
-      <div className="App">
-        <h1>메롱메롱</h1>
-        {$h2}
-      </div>
-      <div className="noName">
-        <input type="text" />
-        <p>
-          오늘은 태풍이와서 비가 많이 내렸다. <br /> 그래서 공부가 하기 싫다.
-        </p>
-      </div>
+      <AppItem />
+      <NoName />
+      <Hello />
     </>
   );
-}
+};
 
 export default App;
---
+++ src/NoName.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function NoName() {
+  return (
+    <div className="noName">
+      <input type="text" />
+      <p>
+        오늘은 태풍이와서 비가 많이 내렸다. <br /> 그래서 공부가 하기 싫다.
+      </p>
+    </div>
+  );
+}
+
+export default NoName;
